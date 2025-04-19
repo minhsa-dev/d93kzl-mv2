c287ac1c6f3eb5eb282b57811a86321958dfab4e
minhsa-dev <mc.minhchau96@gmail.com>
2025-04-19 15:40:42 -0500

    Update EventChannelSO.cs

--- Assets/Scripts/Core/Input/Events/EventChannelSO.cs
+++ Assets/Scripts/Core/Input/Events/EventChannelSO.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 
+// abstract class to allow for different types of events, scriptable objects for modularity, generic event channel value
 public abstract class EventChannelSO<T> : ScriptableObject
 {
     public event Action<T> OnEventRaised;
